81ec51fc305065ea16d93cb546600267aac31bdc
Kaloyan Marinov <kaloyan.marinov@protonmail.com>
2022-04-10 19:14:50 +0300

    get <Login> to work

--- frontend/src/features/Login.tsx
+++ frontend/src/features/Login.tsx
@@ -1,4 +1,18 @@
 import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { Redirect } from "react-router-dom";
+import { ThunkDispatch } from "redux-thunk";
+import { v4 as uuidv4 } from "uuid";
+import {
+  ActionAlerts,
+  ActionFetchProfile,
+  ActionTypesIssueJWSToken,
+  alertsCreate,
+  fetchProfile,
+  issueJWSToken,
+  IState,
+  selectHasValidToken,
+} from "../types";
 
 export const Login = () => {
   const [username, setUsername] = React.useState("");
@@ -7,9 +21,48 @@ export const Login = () => {
     setUsername(e.target.value);
   };
 
+  console.log(
+    `${new Date().toISOString()} - ${__filename} - React is rendering <SignIn>`
+  );
+
+  const hasValidToken: boolean | null = useSelector(selectHasValidToken);
+  console.log("    hasValidToken:");
+  console.log(`    ${hasValidToken}`);
+
+  // const dispatch: ThunkDispatch<
+  //   IState,
+  //   unknown,
+  //   ActionTypesIssueJWSToken | ActionAlerts | ActionFetchProfile
+  // > = useDispatch();
+  const dispatch = useDispatch();
+
+  if (hasValidToken === true) {
+    const nextURL: string = "/";
+    console.log(`    hasValidToken=${hasValidToken} > redirecting to ${nextURL} ...`);
+    return <Redirect to={nextURL} />;
+  }
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    const id: string = uuidv4();
+    if (username === "") {
+      const message: string = "YOU MUST FILL OUT ALL FORM FIELDS";
+      dispatch(alertsCreate(id, message));
+    } else {
+      try {
+        await dispatch(issueJWSToken(username));
+        dispatch(alertsCreate(id, "SIGN-IN SUCCESSFUL"));
+        await dispatch(fetchProfile());
+      } catch (thunkActionError) {
+        dispatch(alertsCreate(id, thunkActionError as string));
+      }
+    }
+  };
+
   return (
     <>
-      <form>
+      <form onSubmit={(e: React.FormEvent<HTMLFormElement>) => handleSubmit(e)}>
         <div>
           <input
             type="text"
--- frontend/src/types.ts
+++ frontend/src/types.ts
@@ -219,7 +219,6 @@ export const createUser = (
       return Promise.resolve();
     } catch (err) {
       if (axios.isAxiosError(err) && err.response) {
-        // console.log(err);
         const responseBody = err.response.data;
         const responseBodyError =
           responseBody.error || responseBody.msg || ERROR_NOT_FROM_BACKEND;
@@ -232,12 +231,186 @@ export const createUser = (
   };
 };
 
+/* authSlice - "auth/issueJWSToken/" action creators */
+export enum ActionTypesIssueJWSToken {
+  PENDING = "auth/issueJWSToken/pending",
+  REJECTED = "auth/issueJWSToken/rejected",
+  FULFILLED = "auth/issueJWSToken/fulfilled",
+}
+
+interface IActionIssueJWSTokenPending {
+  type: typeof ActionTypesIssueJWSToken.PENDING;
+}
+
+interface IActionIssueJWSTokenRejected {
+  type: typeof ActionTypesIssueJWSToken.REJECTED;
+  error: string;
+}
+
+interface IActionIssueJWSTokenFulfilled {
+  type: typeof ActionTypesIssueJWSToken.FULFILLED;
+  payload: {
+    token: string;
+  };
+}
+
+export const issueJWSTokenPending = (): IActionIssueJWSTokenPending => ({
+  type: ActionTypesIssueJWSToken.PENDING,
+});
+
+export const issueJWSTokenRejected = (error: string): IActionIssueJWSTokenRejected => ({
+  type: ActionTypesIssueJWSToken.REJECTED,
+  error,
+});
+
+export const issueJWSTokenFulfilled = (
+  token: string
+): IActionIssueJWSTokenFulfilled => ({
+  type: ActionTypesIssueJWSToken.FULFILLED,
+  payload: {
+    token,
+  },
+});
+
+type ActionIssueJWSToken =
+  | IActionIssueJWSTokenPending
+  | IActionIssueJWSTokenRejected
+  | IActionIssueJWSTokenFulfilled;
+
+/* authSlice - "auth/issueJWSToken" thunk-action creator */
+export const issueJWSToken = (
+  username: string
+): ThunkAction<Promise<any>, IState, unknown, ActionIssueJWSToken> => {
+  /*
+  Create a thunk-action.
+  When dispatched, it issues an HTTP request
+  to the backend's endpoint for issuing a JSON Web Signature token
+  (via which the client can subsequently authenticate itself to the backend
+  application).
+  */
+
+  return async (dispatch: Dispatch<ActionIssueJWSToken>) => {
+    const config = {
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: "Bearer " + username,
+      },
+    };
+
+    const body = {};
+
+    dispatch(issueJWSTokenPending());
+    try {
+      const response = await axios.post("/api/tokens", body, config);
+      localStorage.setItem(NAME_OF_ACCESS_TOKEN, response.data.token);
+      dispatch(issueJWSTokenFulfilled(response.data.token));
+      return Promise.resolve();
+    } catch (err) {
+      if (axios.isAxiosError(err) && err.response) {
+        const responseBody = err.response.data;
+        const responseBodyError =
+          responseBody.error || responseBody.msg || ERROR_NOT_FROM_BACKEND;
+        dispatch(issueJWSTokenRejected(responseBodyError));
+        return Promise.reject(responseBodyError);
+      }
+
+      return Promise.reject(ERROR_NOT_FROM_BACKEND);
+    }
+  };
+};
+
+/* authSlice - "auth/fetchProfile/" action creators */
+enum ActionTypesFetchProfile {
+  PENDING = "auth/fetchProfile/pending",
+  REJECTED = "auth/fetchProfile/rejected",
+  FULFILLED = "auth/fetchProfile/fulfilled",
+}
+
+interface IActionFetchProfilePending {
+  type: typeof ActionTypesFetchProfile.PENDING;
+}
+
+interface IActionFetchProfileRejected {
+  type: typeof ActionTypesFetchProfile.REJECTED;
+  error: string;
+}
+
+interface IActionFetchProfileFulfilled {
+  type: typeof ActionTypesFetchProfile.FULFILLED;
+  payload: {
+    profile: IProfile;
+  };
+}
+
+export const fetchProfilePending = (): IActionFetchProfilePending => ({
+  type: ActionTypesFetchProfile.PENDING,
+});
+
+export const fetchProfileRejected = (error: string): IActionFetchProfileRejected => ({
+  type: ActionTypesFetchProfile.REJECTED,
+  error,
+});
+
+export const fetchProfileFulfilled = (
+  profile: IProfile
+): IActionFetchProfileFulfilled => ({
+  type: ActionTypesFetchProfile.FULFILLED,
+  payload: {
+    profile,
+  },
+});
+
+export type ActionFetchProfile =
+  | IActionFetchProfilePending
+  | IActionFetchProfileRejected
+  | IActionFetchProfileFulfilled;
+
+/* authSlice - "auth/fetchProfile" thunk-action creator */
+export const fetchProfile = (): ThunkAction<
+  Promise<any>,
+  IState,
+  unknown,
+  ActionFetchProfile
+> => {
+  /*
+  Create a thunk-action.
+  When dispatched, it makes the web browser issue an HTTP request
+  to the backend's endpoint for fetching the Profile of a specific User.
+  That User is uniquely specified by JSON Web Signature token,
+  which is required to have been saved earlier (by the frontend)
+  in the HTTP-request-issuing web browser.
+  */
+
+  return async (dispatch) => {
+    const config = {
+      headers: {
+        Authorization: "Bearer " + localStorage.getItem(NAME_OF_ACCESS_TOKEN),
+      },
+    };
+
+    dispatch(fetchProfilePending());
+    try {
+      const response = await axios.get("/api/user-profile", config);
+      dispatch(fetchProfileFulfilled(response.data));
+      return Promise.resolve();
+    } catch (err) {
+      if (axios.isAxiosError(err) && err.response) {
+        const responseBody = err.response.data;
+        const responseBodyError =
+          responseBody.error || responseBody.msg || ERROR_NOT_FROM_BACKEND;
+        dispatch(fetchProfileRejected(responseBodyError));
+        return Promise.reject(responseBodyError);
+      }
+
+      return Promise.reject(ERROR_NOT_FROM_BACKEND);
+    }
+  };
+};
+
 /* authSlice - reducer */
 export const authReducer = (
   stateAuth: IStateAuth = initialStateAuth,
-  action: ActionCreateUser
-  // | ActionIssueJWSToken
-  // | ActionFetchProfile
+  action: ActionCreateUser | ActionIssueJWSToken | ActionFetchProfile
   // | IActionClearAuthSlice
 ): IStateAuth => {
   switch (action.type) {
@@ -262,56 +435,56 @@ export const authReducer = (
         requestError: null,
       };
 
-    // case ActionTypesIssueJWSToken.PENDING:
-    //   return {
-    //     ...stateAuth,
-    //     requestStatus: RequestStatus.LOADING,
-    //     requestError: null,
-    //   };
+    case ActionTypesIssueJWSToken.PENDING:
+      return {
+        ...stateAuth,
+        requestStatus: RequestStatus.LOADING,
+        requestError: null,
+      };
 
-    // case ActionTypesIssueJWSToken.REJECTED:
-    //   return {
-    //     ...stateAuth,
-    //     requestStatus: RequestStatus.FAILED,
-    //     requestError: action.error,
-    //     hasValidToken: false,
-    //   };
+    case ActionTypesIssueJWSToken.REJECTED:
+      return {
+        ...stateAuth,
+        requestStatus: RequestStatus.FAILED,
+        requestError: action.error,
+        hasValidToken: false,
+      };
 
-    // case ActionTypesIssueJWSToken.FULFILLED:
-    //   return {
-    //     ...stateAuth,
-    //     requestStatus: RequestStatus.SUCCEEDED,
-    //     requestError: null,
-    //     token: action.payload.token,
-    //     hasValidToken: true,
-    //   };
+    case ActionTypesIssueJWSToken.FULFILLED:
+      return {
+        ...stateAuth,
+        requestStatus: RequestStatus.SUCCEEDED,
+        requestError: null,
+        token: action.payload.token,
+        hasValidToken: true,
+      };
 
-    // case ActionTypesFetchProfile.PENDING:
-    //   return {
-    //     ...stateAuth,
-    //     requestStatus: RequestStatus.LOADING,
-    //     requestError: null,
-    //   };
+    case ActionTypesFetchProfile.PENDING:
+      return {
+        ...stateAuth,
+        requestStatus: RequestStatus.LOADING,
+        requestError: null,
+      };
 
-    // case ActionTypesFetchProfile.REJECTED:
-    //   return {
-    //     ...stateAuth,
-    //     requestStatus: RequestStatus.FAILED,
-    //     requestError: action.error,
-    //     hasValidToken: false,
-    //   };
+    case ActionTypesFetchProfile.REJECTED:
+      return {
+        ...stateAuth,
+        requestStatus: RequestStatus.FAILED,
+        requestError: action.error,
+        hasValidToken: false,
+      };
 
-    // case ActionTypesFetchProfile.FULFILLED: {
-    //   const profile: IProfile = action.payload.profile;
+    case ActionTypesFetchProfile.FULFILLED: {
+      const profile: IProfile = action.payload.profile;
 
-    //   return {
-    //     ...stateAuth,
-    //     requestStatus: RequestStatus.SUCCEEDED,
-    //     requestError: null,
-    //     hasValidToken: true,
-    //     signedInUserProfile: profile,
-    //   };
-    // }
+      return {
+        ...stateAuth,
+        requestStatus: RequestStatus.SUCCEEDED,
+        requestError: null,
+        hasValidToken: true,
+        signedInUserProfile: profile,
+      };
+    }
 
     // case ACTION_TYPE_CLEAR_AUTH_SLICE:
     //   return {
