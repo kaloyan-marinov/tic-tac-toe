4d8dd6e285afa920448bd7d1caaa447d85b45bcf
Kaloyan Marinov <kaloyan.marinov@protonmail.com>
2022-04-10 09:52:53 +0300

    `npm install --save-dev supertest @types/supertest` + replace the simple test with a test of the web application

--- backend/__tests__/server.test.ts
+++ backend/__tests__/server.test.ts
@@ -1,3 +1,29 @@
-test("GET /", () => {
-  expect(2 + 2).toEqual(4);
+import http from "http";
+
+import request from "supertest";
+
+import { app } from "../src/application";
+
+let server: http.Server;
+
+beforeEach(() => {
+  const port: number = 3001;
+  server = http.createServer(app.callback());
+  server.listen(port, () => {
+    console.log(`Server listening on port ${port} ...`);
+  });
+});
+
+afterEach(() => {
+  server.close();
+});
+
+test("GET /", async () => {
+  const response = await request(server).get("/");
+
+  expect(response.status).toEqual(200);
+  expect(response.type).toEqual("application/json");
+  expect(response.body).toEqual({
+    msg: "Hello World!",
+  });
 });
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,9 +1,10 @@
 import http from "http";
 
+import { PORT } from "./constants";
 import { app } from "./application";
 
 const server: http.Server = http.createServer(app.callback());
 
-server.listen(3000, () => {
-  console.log(`Server listening on port 3000`);
+server.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT} ...`);
 });
