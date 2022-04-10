5bb052c117d92dd08732679d2cc53f3ec54dc2c7
Kaloyan Marinov <kaloyan.marinov@protonmail.com>
2022-04-10 17:07:27 +0300

    add a form to <Register>

--- frontend/src/features/Register.tsx
+++ frontend/src/features/Register.tsx
@@ -1,3 +1,26 @@
+import React from "react";
+
 export const Register = () => {
-  return <div>Register</div>;
+  const [username, setUsername] = React.useState("");
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setUsername(e.target.value);
+  };
+
+  return (
+    <>
+      <form>
+        <div>
+          <input
+            type="text"
+            placeholder="Enter username"
+            name="username"
+            value={username}
+            onChange={(e) => handleChange(e)}
+          />
+        </div>
+        <input type="submit" value="Register" />
+      </form>
+    </>
+  );
 };
